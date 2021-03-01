3bc432976d6548619f5a27f71f7921d83e57e930
Alex Dudley <alexdudley@me.com>
2021-03-01 14:04:34 -0600

    Add: added my country data arrays

---
+++ scripts/thailand/landmarks/LandmarkData.js
@@ -0,0 +1,14 @@
+const landmarkCollection = [
+    {
+        landmarkName: "Wat Arun"
+    },
+    {
+        landmarkName: "Wat Prathat Doi Suthep"
+    },
+    {
+        landmarkName: "Wat Rong Khun"
+    },
+    {
+        landmarkName: "Bridge Over the River Kwai"
+    }
+]
