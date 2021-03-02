d797d810a4c50f01c949426bb8bb57eaf5543df5
Alex Dudley <alexdudley@me.com>
2021-03-02 11:03:45 -0600

    added Thailand.html

--- scripts/main.js
+++ scripts/main.js
@@ -21,8 +21,7 @@ thailandLandmarkList();
 
 import { thailandCityList } from './thailand/cities/CityList.js';
 thailandCityList();
-=======
-//import { CelebList } from './thailand/celebrities/CelebrityList.js';
-//CelebList();
+
+
 
 
