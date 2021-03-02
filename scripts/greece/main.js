c8da2e565d1bbc9afeb4e78d2aff13af8fc54efe
brandonjon26 <brandonjon26@gmail.com>
2021-03-02 14:19:12 -0600

    fixed pathways for main.js

---
+++ scripts/greece/main.js
@@ -0,0 +1,8 @@
+console.log("Visit this country!")
+
+import { greeceCelebritiesList } from "./celebrities/celebritiesList.js";
+import { greeceCitiesList } from "./cities/citiesList.js";
+import { greeceLandmarksList } from "./landmarks/landmarksList.js";
+greeceCelebritiesList();
+greeceCitiesList();
+greeceLandmarksList();
